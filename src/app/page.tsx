75d8c37152b9922331e8f7318f658f5a4f8ebffd
sergiokano <sergiocano.dev@gmail.com>
2025-04-24 21:29:50 +0200

    fix: change filter to work correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import PokemonCard from "@/components/PokemonCard";
 import { useEffect, useState } from "react";
 import FilterBar from "@/components/FilterBar";
 import { useFullPokemonIndex } from "@/hooks/useFullPokemonIndex";
-import { fetchPokemonByName } from "@/lib/api";
+import { normalizePokemonData } from "@/lib/api";
 
 export default function HomePage() {
   const { data: visiblePokemon, isLoading, error } = usePokemonList();
@@ -29,7 +29,7 @@ export default function HomePage() {
       (name) => !visiblePokemon.some((p) => p.name === name),
     );
 
-    Promise.all(newOnes.map(fetchPokemonByName))
+    Promise.all(newOnes.map(normalizePokemonData))
       .then(setExtraResults)
       .catch(console.error);
   }, [search, fullIndex, visiblePokemon]);
@@ -37,15 +37,17 @@ export default function HomePage() {
   const allPokemon = [...(visiblePokemon || []), ...extraResults];
 
   const filtered = allPokemon.filter((p) => {
+    console.log("checking", p);
+
     const nameMatch = p.name.toLowerCase().includes(search.toLowerCase());
     const familyMatch =
       fullIndex
         ?.find((f) => f.name === p.name)
         ?.family.some((n) => n.includes(search.toLowerCase())) ?? false;
-
+    console.log(p.types, p.generation);
     const typeMatch = typeFilter ? p.types?.includes(typeFilter) : true;
     const genMatch = generationFilter
-      ? p.generation === generationFilter
+      ? p.generation?.toLowerCase() === generationFilter.toLowerCase()
       : true;
 
     return (nameMatch || familyMatch) && typeMatch && genMatch;
@@ -65,7 +67,7 @@ export default function HomePage() {
         generationFilter={generationFilter}
         onGenerationFilter={setGenerationFilter}
       />
-      <div className="w-full ">
+      <div className="w-full">
         <section className="grid grid-cols-[repeat(auto-fill,_minmax(220px,_1fr))] gap-6">
           {filtered?.map((pokemon: any) => (
             <PokemonCard key={pokemon.id} {...pokemon} />
