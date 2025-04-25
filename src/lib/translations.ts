ed8fccdcc8d10c99d728e2c5ece869318fc99c54
sergiokano <sergiocano.dev@gmail.com>
2025-04-25 02:37:03 +0200

    feat: add translations

---
+++ src/lib/translations.ts
@@ -0,0 +1,44 @@
+export const statTranslations: Record<string, string> = {
+    hp: "PS",
+    attack: "Ataque",
+    defense: "Defensa",
+    "special-attack": "Ataque Especial",
+    "special-defense": "Defensa Especial",
+    speed: "Velocidad",
+  };
+  
+  export const generationTranslations: Record<string, string> = {
+    "generation-i": "1ª Generación",
+    "generation-ii": "2ª Generación",
+    "generation-iii": "3ª Generación",
+    "generation-iv": "4ª Generación",
+    "generation-v": "5ª Generación",
+    "generation-vi": "6ª Generación",
+    "generation-vii": "7ª Generación",
+    "generation-viii": "8ª Generación",
+    "generation-ix": "9ª Generación",
+  };
+  
+  export const typeTranslations: Record<string, string> = {
+    normal: "Normal",
+    fighting: "Lucha",
+    flying: "Volador",
+    poison: "Veneno",
+    ground: "Tierra",
+    rock: "Roca",
+    bug: "Bicho",
+    ghost: "Fantasma",
+    steel: "Acero",
+    fire: "Fuego",
+    water: "Agua",
+    grass: "Planta",
+    electric: "Eléctrico",
+    psychic: "Psíquico",
+    ice: "Hielo",
+    dragon: "Dragón",
+    dark: "Siniestro",
+    fairy: "Hada",
+    unknown: "Desconocido",
+    shadow: "Sombra",
+  };
+  
