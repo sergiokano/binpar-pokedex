f299cd02037455ec44611b2073d77583547d2b7e
sergiokano <sergiocano.dev@gmail.com>
2025-04-25 02:43:09 +0200

    feat: add emojies to type translations

--- src/lib/translations.ts
+++ src/lib/translations.ts
@@ -1,44 +1,42 @@
 export const statTranslations: Record<string, string> = {
-    hp: "PS",
-    attack: "Ataque",
-    defense: "Defensa",
-    "special-attack": "Ataque Especial",
-    "special-defense": "Defensa Especial",
-    speed: "Velocidad",
-  };
-  
-  export const generationTranslations: Record<string, string> = {
-    "generation-i": "1ª Generación",
-    "generation-ii": "2ª Generación",
-    "generation-iii": "3ª Generación",
-    "generation-iv": "4ª Generación",
-    "generation-v": "5ª Generación",
-    "generation-vi": "6ª Generación",
-    "generation-vii": "7ª Generación",
-    "generation-viii": "8ª Generación",
-    "generation-ix": "9ª Generación",
-  };
-  
-  export const typeTranslations: Record<string, string> = {
-    normal: "Normal",
-    fighting: "Lucha",
-    flying: "Volador",
-    poison: "Veneno",
-    ground: "Tierra",
-    rock: "Roca",
-    bug: "Bicho",
-    ghost: "Fantasma",
-    steel: "Acero",
-    fire: "Fuego",
-    water: "Agua",
-    grass: "Planta",
-    electric: "Eléctrico",
-    psychic: "Psíquico",
-    ice: "Hielo",
-    dragon: "Dragón",
-    dark: "Siniestro",
-    fairy: "Hada",
-    unknown: "Desconocido",
-    shadow: "Sombra",
-  };
-  
+  hp: "PS",
+  attack: "Ataque",
+  defense: "Defensa",
+  "special-attack": "Ataque Especial",
+  "special-defense": "Defensa Especial",
+  speed: "Velocidad",
+};
+
+export const generationTranslations: Record<string, string> = {
+  "generation-i": "1ª Generación",
+  "generation-ii": "2ª Generación",
+  "generation-iii": "3ª Generación",
+  "generation-iv": "4ª Generación",
+  "generation-v": "5ª Generación",
+  "generation-vi": "6ª Generación",
+  "generation-vii": "7ª Generación",
+  "generation-viii": "8ª Generación",
+  "generation-ix": "9ª Generación",
+};
+
+export const typeTranslations: Record<string, string> = {
+  normal: "⚪ Normal",
+  fighting: "🥊 Lucha",
+  flying: "🌬️ Volador",
+  poison: "☠️ Veneno",
+  ground: "🌍 Tierra",
+  rock: "🪨 Roca",
+  bug: "🐛 Bicho",
+  ghost: "👻 Fantasma",
+  steel: "⚙️ Acero",
+  fire: "🔥 Fuego",
+  water: "💧 Agua",
+  grass: "🌿 Planta",
+  electric: "⚡ Eléctrico",
+  psychic: "🔮 Psíquico",
+  ice: "❄️ Hielo",
+  dragon: "🐉 Dragón",
+  dark: "🌑 Siniestro",
+  fairy: "🧚 Hada",
+  shadow: "🌫️ Sombra",
+};
