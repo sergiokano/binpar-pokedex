3fc8b96fd9e7c7a0a2903e57d275078110cd55e3
sergiokano <sergiocano.dev@gmail.com>
2025-04-25 03:54:46 +0200

    style: adjusting id position in pokemon card

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -35,6 +35,11 @@ export default function PokemonCard({
   return (
     <Link href={`/pokemon/${name}?${query}`}>
       <div className="group relative cursor-pointer rounded-xl border border-gray-200 bg-gradient-to-br from-white to-gray-100 p-4 shadow-md transition-all duration-300 hover:scale-105 hover:shadow-xl">
+        {/* ID en esquina superior derecha */}
+        <span className="absolute right-2 top-2 rounded-full bg-white/80 px-2 py-1 text-xs font-semibold text-zinc-500 shadow-sm backdrop-blur">
+          #{id}
+        </span>
+
         <div className="flex flex-col items-center">
           <img
             src={sprite}
@@ -42,7 +47,7 @@ export default function PokemonCard({
             className="h-24 w-auto max-w-[100px] object-contain transition-transform duration-300 group-hover:scale-110"
           />
           <p className="mt-2 text-center text-lg font-bold text-gray-800 capitalize">
-            #{id} {name}
+            {name}
           </p>
           <p className="text-sm text-gray-500">
             Gen {generation.replace("generation-", "").toUpperCase()}
